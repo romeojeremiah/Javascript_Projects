d44806e6e6f773a86b7cee8e63df2cd91289e0f7
Romeo <romeojeremiah@gmail.com>
2019-05-12 13:15:43 -0500

    add second part of filter project

--- _Filter_Project/js/app.js
+++ _Filter_Project/js/app.js
@@ -73,4 +73,26 @@
         })
     })
 
-})()
+})();
+
+//wire up filter search box
+(function(){
+
+    const searchBox = document.querySelector('#search-item')
+    const storeItems = document.querySelectorAll('.store-item')
+
+    searchBox.addEventListener('keyup', (e) => {
+    
+        const searchFilter = e.target.value.toLowerCase().trim()
+        //display only items that contain filter input
+
+        storeItems.forEach((item) => {
+            if (item.textContent.includes(searchFilter)){
+                item.style.display = 'block'
+            } else {
+                item.style.display = 'none'
+            }
+        })
+    })
+
+})();
