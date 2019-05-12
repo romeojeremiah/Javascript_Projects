0681b71e6dd9dddb8171110f4c4849ceac88dcb3
Romeo <romeojeremiah@gmail.com>
2019-05-12 12:38:42 -0500

    refactor first part code of filter project

--- _Filter_Project/js/app.js
+++ _Filter_Project/js/app.js
@@ -1,52 +1,75 @@
 // Work to get the filter buttons working
 (function(){
+    // const buttons = document.querySelectorAll('.btn')
+    // const storeItems = document.querySelectorAll('.store-item')
+    //console.log(buttons)
+    // buttons.forEach(function(button){
+    //     button.addEventListener('click', function(e){
+    //         //prevent the default link jump to top of page
+    //         e.preventDefault()
+    //         //grab the filter button that was clicked
+    //         const filter = e.target.dataset.filter
+    //         if (filter === 'all'){
+    //             //show all items
+    //             storeItems.forEach(function(item){
+    //                 item.style.display = 'block'
+    //             })
+    //         } else if (filter === 'cakes'){
+    //             storeItems.forEach(function(item){
+    //                 if (item.classList.contains('cakes')){
+    //                     item.style.display = 'block'
+    //                 } else {
+    //                     item.style.display = 'none'
+    //                 }
+    //             })
+    //         } else if (filter === 'cupcakes'){
+    //             storeItems.forEach(function(item){
+    //                 if (item.classList.contains('cupcakes')){
+    //                     item.style.display = 'block'
+    //                 } else {
+    //                     item.style.display = 'none'
+    //                 }
+    //             })
+    //         } else if (filter === 'sweets'){
+    //             storeItems.forEach(function(item){
+    //                 if (item.classList.contains('sweets')){
+    //                     item.style.display = 'block'
+    //                 } else {
+    //                     item.style.display = 'none'
+    //                 }
+    //             })
+    //         } else if (filter === 'doughnuts'){
+    //             storeItems.forEach(function(item){
+    //                 if (item.classList.contains('doughnuts')){
+    //                     item.style.display = 'block'
+    //                 } else {
+    //                     item.style.display = 'none'
+    //                 }
+    //             })
+    //         }
+    //     })
+    // })
+
+    // refactor to get rid of DRY code
     const buttons = document.querySelectorAll('.btn')
     const storeItems = document.querySelectorAll('.store-item')
-    //console.log(buttons)
-    buttons.forEach(function(button){
-        button.addEventListener('click', function(e){
-            //prevent the default link jump to top of page
+
+    buttons.forEach((button)=> {
+        button.addEventListener('click', (e) => {
             e.preventDefault()
-            //grab the filter button that was clicked
             const filter = e.target.dataset.filter
-            if (filter === 'all'){
-                //show all items
-                storeItems.forEach(function(item){
+            
+            storeItems.forEach((item)=> {
+                if (filter === 'all'){
                     item.style.display = 'block'
-                })
-            } else if (filter === 'cakes'){
-                storeItems.forEach(function(item){
-                    if (item.classList.contains('cakes')){
+                } else {
+                    if (item.classList.contains(filter)){
                         item.style.display = 'block'
                     } else {
                         item.style.display = 'none'
                     }
-                })
-            } else if (filter === 'cupcakes'){
-                storeItems.forEach(function(item){
-                    if (item.classList.contains('cupcakes')){
-                        item.style.display = 'block'
-                    } else {
-                        item.style.display = 'none'
-                    }
-                })
-            } else if (filter === 'sweets'){
-                storeItems.forEach(function(item){
-                    if (item.classList.contains('sweets')){
-                        item.style.display = 'block'
-                    } else {
-                        item.style.display = 'none'
-                    }
-                })
-            } else if (filter === 'doughnuts'){
-                storeItems.forEach(function(item){
-                    if (item.classList.contains('doughnuts')){
-                        item.style.display = 'block'
-                    } else {
-                        item.style.display = 'none'
-                    }
-                })
-            }
+                }
+            })
         })
     })
 
